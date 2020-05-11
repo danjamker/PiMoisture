db35e2eafe5eb8eca80216c352807af076d0c6b3
Daniel Kershaw <905890+danjamker@users.noreply.github.com>
2020-05-11 20:49:39 +0100

    CouchDB
    Move from Mongo to Couch

--- app/tasks.py
+++ app/tasks.py
@@ -1,12 +1,12 @@
 import pika, os, json
 from datetime import datetime
-import pymongo
+from couchdb import Server, Document
+from couchdb.mapping import TextField, IntegerField, DateTimeField, FloatField
 
 url = os.environ.get('CELERY_BROKER_URL')
 params = pika.URLParameters(url)
 connection = pika.BlockingConnection(params)
 channel = connection.channel() # start a channel
-channel.queue_declare(queue='weather') # Declare a queue
 
 def store(ch, method, properties, body):
     print(ch)
@@ -17,25 +17,30 @@ def store(ch, method, properties, body):
     for v in body:
         insert_value(v)
 
+class Record(Document):
+    host = TextField()
+    messurement = FloatField()
+    time = DateTimeField(default=datetime.now())
 
 def insert_value(body):
     print("[X] Received time:" + str(body["t"]) + " and temperature: " + str(body["T"]))
     try:
-        client = pymongo.MongoClient(os.environ["MONGO_URI"])
-        db = client["home"]
-        col = db["plant"]
+        print(os.environ["COUCH_URI"])
+        couch = Server("http://admin:couchdb@couchdb:5984/")
+        dbname = "plant"
+        if dbname in couch:
+            db = couch[dbname]
+        else:
+            db = couch.create(dbname)
 
-        result = col.insert(
-            {"messurement": "moisture", "value": body["T"], "host": body["h"], "time": datetime.fromtimestamp(body["t"])})
-        print(result, "Record inserted successfully into weather table")
+        messurment = Record(host=body["h"], messurement=body["T"])
+        db.save(messurment)
 
-    except (Exception, pymongo.errors) as error:
-        print("Error while connecting to MongoDB", error)
-    finally:
-        # closing database connection.
-        if (client):
-            client.close()
-            print("MongoDB connection is closed")
+        print(messurment, "Record inserted successfully into weather table")
+
+    except Exception as error:
+        print("Error while connecting to CouchDB", error)
+
 
 
 channel.basic_consume('moisture', store, auto_ack=True)
